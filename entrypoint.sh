cec627de9b3a1bf0cce0abe5f96960c633d1780d
Christian Hase <hase.christian92@gmail.com>
2023-12-15 22:06:00 +0100

    first entrypoint skript test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,18 @@
 #!/dumb-init /entrypoint.sh
+options=""
+
+# get nic interfaces
+nics=$(ls /sys/class/net/ | grep -i "eth" )
+
+# check the needed options
+if [[ -z "${UBR_DEBUG}" ]]; then
+    options+="-d "
+fi
+
+if [[ -z $(cat /etc/passwd | grep -i "serviceuser") ]]; then
+    options+="-u serviceuser "
+fi
 
 
+# run udp relay
+/udp-broadcast-relay $options $UBR_ID $UBR_PORT $nics
